0c43c90ed1ed47cfaedcad487824d8157a58998a
Eric Atempa <ea242@njit.edu>
2017-04-07 15:55:41 -0400

    Done HW15

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="https://web.njit.edu/~ea242/IS218/HW15/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Guitar Shop</h1></header>
